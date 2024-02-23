6bc811f463d25ba6ccda3778da57299e5d985ba5
Matt Kadacham <matt.kadacham@gmail.com>
2024-02-23 22:33:35 +0000

    jkhkljhlk

--- plugin/copy.ps1
+++
@@ -1,8 +0,0 @@
-
-$filepath = "C:\dev\notes\.obsidian\plugins\obsync-obsidian-plugin"
-
-if (-not (Test-Path -Path $filepath)) {
-    New-Item -ItemType Directory -Path $filepath | Out-Null
-}
-
-Copy-Item -Path "main.js", "manifest.json", "styles.css" -Destination $filepath
